e84b045bdfde9ab97798f53d61976006bf45df10
Konrad Kraszewski <laiquendir@gmail.com>
2024-05-13 23:29:18 +0200

    picom gestures

--- update.sh
+++ update.sh
@@ -4,3 +4,5 @@ cp ~/.xprofile .
 cp -r ~/.config/i3 i3
 cp /usr/share/X11/xorg.conf.d/40-libinput.conf .
 cp ~/.config/nvim/init.lua .
+cp ~/.config/picom/picom.lua .
+cp /etc/libinput-gestures.conf .
